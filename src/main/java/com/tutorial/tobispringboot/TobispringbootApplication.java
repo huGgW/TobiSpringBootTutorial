2b3a07b01e00dc831f05852208f06ac206915660
huGgW <whjoon0225@naver.com>
2024-03-01 21:20:04 +0900

    init

---
+++ src/main/java/com/tutorial/tobispringboot/TobispringbootApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.tobispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobispringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TobispringbootApplication.class, args);
+	}
+
+}
